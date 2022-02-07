700e051891975f17cb6af3601c383f2f341d6f20
AurelioRS <93615186+AurelioRS@users.noreply.github.com>
2022-02-07 20:21:46 +0100

    Add files via upload

---
+++ Boletin4/src/test/java/es/iessoterohernandez/daw/endes/Boletin4/AllTests.java
@@ -0,0 +1,12 @@
+package es.iessoterohernandez.daw.endes.Boletin4;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestAccount.class, TestBoa.class, TestFridge.class, TestOperadorAritmetico.class, TestPila.class,
+		TestSubscripcion.class })
+public class AllTests {
+
+}
